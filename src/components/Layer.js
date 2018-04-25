2a575f4c8e60b61890c6a1107051a2fef39ccb81
daryl <darylcoleoffice@gmail.com>
2018-04-25 10:13:20 +0900

    tidy up layer file

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -25,6 +25,10 @@ const AddButton = styled.button`
 	margin-right: 5px;
 `
 
+const LayerItem = styled.li`
+    display: inline;
+`
+
 class Layer extends Component {
 
 
@@ -32,49 +36,52 @@ class Layer extends Component {
         const {layer, index, handleChange, handleColorChange, addColor, handleColorAmountChange, removeColor, checked} = this.props;
         return (
             <span>
-            <LayerLabel>{`Layer ${index}`}</LayerLabel>
-            <label style={{display: "inline"}}>Hide Layer<input
-							name="hidden"
-							id={index}
-							type="checkbox"
-							checked={checked}
-							onChange={handleChange} /></label>
-            <AngleSlider>
-                <label>Angle</label>
-                <input type="range" min="0" max="359" name="degree" id={index} value={layer.degree} onChange={handleChange} />
-            </AngleSlider>
-            <ul style={{"list-style-type": "none", margin: "0", padding: "0", paddingLeft: "10px"}}>
-            {layer.colors.map(color => {
-                return <div>
-                        <li style={{display: "inline"}}>
-                            <ColorPicker
-                                h={color.h}
-                                s={color.s}
-                                l={color.l}
-                                a={color.a}
-                                amount={color.amount}
-                                name={color.id}
-                                handleChange={handleColorChange}>
-                            </ColorPicker>
-                        </li>
-                        <li style={{display: "inline"}}>
-                        <input
-							type="range"
-							min="0"
-							max="100"
-							value={color.amount}
-							name={color.id}
-							onChange={handleColorAmountChange} />
-						</li>
-                        <li style={{display: "inline"}}>
-							<AddButton onClick={removeColor} name={color.id} id={index}> 
-							    <i class="fas fa-trash" />
-							</AddButton>
-                        </li>
-                        </div>
-                        })}
-            <li>{layer.colors.length < 3 && index != undefined ? (<AddButton name={index} onClick={addColor}><i class="fas fa-plus" />Color</AddButton>) : null}</li>
-            </ul>
+                <LayerLabel>{`Layer ${index}`}</LayerLabel>
+                <label>
+                    Hide Layer
+                    <input
+                        name="hidden"
+                        id={index}
+                        type="checkbox"
+                        checked={checked}
+                        onChange={handleChange} />
+                </label>
+                <AngleSlider>
+                    <label>Angle</label>
+                    <input type="range" min="0" max="359" name="degree" id={index} value={layer.degree} onChange={handleChange} />
+                </AngleSlider>
+                <ul style={{"list-style-type": "none", margin: "0", padding: "0", paddingLeft: "10px"}}>
+                    {layer.colors.map(color => {
+                        return <span>
+                                    <LayerItem>
+                                        <ColorPicker
+                                            h={color.h}
+                                            s={color.s}
+                                            l={color.l}
+                                            a={color.a}
+                                            amount={color.amount}
+                                            name={color.id}
+                                            handleChange={handleColorChange}>
+                                        </ColorPicker>
+                                    </LayerItem>
+                                    <LayerItem>
+                                        <input
+                                            type="range"
+                                            min="0"
+                                            max="100"
+                                            value={color.amount}
+                                            name={color.id}
+                                            onChange={handleColorAmountChange} />
+                                    </LayerItem>
+                                    <LayerItem>
+                                        <AddButton onClick={removeColor} name={color.id} id={index}> 
+                                            <i class="fas fa-trash" />
+                                        </AddButton>
+                                    </LayerItem>
+                                </span>
+                                })}
+                    <li>{layer.colors.length < 3 && index != undefined ? (<AddButton name={index} onClick={addColor}><i class="fas fa-plus" />Color</AddButton>) : null}</li>
+                </ul>
             </span>
         );
     }
