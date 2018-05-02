bc676094a58bcf3f16e87627ec241c644741c178
daryl <darylcoleoffice@gmail.com>
2018-05-02 14:43:23 +0900

    Update slider style

--- src/components/ColorDistSlider.js
+++ src/components/ColorDistSlider.js
@@ -11,7 +11,6 @@ class ColorDistSlider extends Component {
     componentDidMount = () => {
         const { colors } = this.props;
         let a = colors.map(color => (color.amount));
-        console.log(a)
         this.setState({colors: colors});
     }
 
@@ -32,10 +31,47 @@ class ColorDistSlider extends Component {
         handleColorAmountChange(layer, this.state.colors)  
     }
 
+    getHandleColors = () => {
+        let handleVals = []
+        this.state.colors.forEach((color, index) => {
+            handleVals.push({backgroundColor: 
+                `hsla(${this.state.colors[index].h}, ${this.state.colors[index].s}%, ${this.state.colors[index].l}%, ${this.state.colors[index].a})`
+            });
+        });
+        return handleVals;
+    }
+
+    getTrackStyle = () => {
+        let trackVals = []
+        this.state.colors.forEach(color => {
+            trackVals.push({backgroundColor: `#abe2fb00`})
+        })
+        return trackVals
+    }
+
+    getRailStyle = () => {
+        let str = '';
+        str+= `linear-gradient(90deg, `;
+        this.state.colors.forEach((color, index) => {
+            str+= `hsla(${color.h}, ${color.s}%, ${color.l}%,  ${color.a}) ${color.amount}%`
+        	str+= index === this.state.colors.length-1 ? `)` : ',';
+        });
+        return str
+    }
+
     render() {
         return (
             <div>
-                {this.state.colors.length > 0 ? <Range min={0} max={100} value={this.state.colors.map(color => color.amount)} onChange={this.handleChange} allowCross={false}/> : null}
+                {this.state.colors.length > 0 ? <Range 
+                min={0} 
+                max={100}  
+                pushable={5} 
+                value={this.state.colors.map(color => color.amount)} 
+                handleStyle={this.getHandleColors()} 
+                trackStyle={this.getTrackStyle()}
+                railStyle={{background: this.getRailStyle(), height: `20px`}} 
+                onChange={this.handleChange} 
+                allowCross={false}/> : null}
             </div>
         );
     }
