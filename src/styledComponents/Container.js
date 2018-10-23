46ab2c7fcf5c67e40fce9983c4469b078097261c
daryl <darylcoleoffice@gmail.com>
2018-10-23 21:57:34 +0900

    Reset dom and remove unused imports

--- src/styledComponents/Container.js
+++ src/styledComponents/Container.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const Container = styled.div`
 	width: 85vw;  
+	height: 75vh;
 	margin: auto;
-	margin-top: 5%;
 `
 
 export default Container;
