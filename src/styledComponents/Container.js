39d15fef0104f4ce7b2484bff792001e87f28ab5
daryl <darylcoleoffice@gmail.com>
2018-11-11 00:46:19 +0900

    👩🏼‍🎨 Position contain absolutely

--- src/styledComponents/Container.js
+++ src/styledComponents/Container.js
@@ -12,11 +12,16 @@ to {
 
 
 const Container = styled.div`
-	width: 70vw;
+	width: 80vw;
 	animation: ${fadeIn} .3s linear;
 	opacity: 1;
 	transition: opacity .2s ease-out;
-	margin: auto;
+	position: absolute; 
+	left: 0; 
+	right: 0; 
+	margin-left: auto; 
+	margin-right: auto; 
+	bottom: 5%;
 
 	${ props => props.hide && css`
 		opacity: 0;
