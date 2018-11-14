539323ff3fbf7dc2dcf638d8b6002bab21fedc41
daryl <darylcoleoffice@gmail.com>
2018-11-14 09:58:30 +0900

    Updates

---
+++ precache-manifest.2c6384052890dbba2d5181dda6e00749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba141f0785a424b6ba11",
+    "url": "/gradientr/static/js/runtime~main.ba141f07.js"
+  },
+  {
+    "revision": "616cf9b6311def50d387",
+    "url": "/gradientr/static/js/main.616cf9b6.chunk.js"
+  },
+  {
+    "revision": "58363a37dd45cd04e198",
+    "url": "/gradientr/static/js/1.58363a37.chunk.js"
+  },
+  {
+    "revision": "616cf9b6311def50d387",
+    "url": "/gradientr/static/css/main.f9452ec4.chunk.css"
+  },
+  {
+    "revision": "58363a37dd45cd04e198",
+    "url": "/gradientr/static/css/1.132156a6.chunk.css"
+  },
+  {
+    "revision": "876e8f3f70fe832a75c4355b1d95d605",
+    "url": "/gradientr/index.html"
+  }
+];
